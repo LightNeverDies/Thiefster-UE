b3ad575827cbec7bfe6dcf9c4a7fc45b0ca837d6
StormGrace <nightavenger54@abv.bg>
2018-09-22 19:29:12 +0300

    Level Design Foundations
    Walls, Floors and Doors has been added.

---
+++ Source/MyProject/CameraProjection.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CameraProjection.h"
+
+CameraProjection::CameraProjection()
+{
+
+}
+
+CameraProjection::~CameraProjection()
+{
+
+}
